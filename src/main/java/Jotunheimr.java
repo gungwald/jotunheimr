4b939c9f90e458d55ead4f7e36191cac4e1e91f0
Bill Chatfield <bill_chatfield@yahoo.com>
2017-12-01 02:57:56 -0500

    Added some real code

--- src/main/java/Jotunheimr.java
+++ src/main/java/Jotunheimr.java
@@ -1,4 +1,54 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.gui2.DefaultWindowManager;
+import com.googlecode.lanterna.gui2.Direction;
+import com.googlecode.lanterna.gui2.EmptySpace;
+import com.googlecode.lanterna.gui2.LinearLayout;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.gui2.Panel;
+import com.googlecode.lanterna.gui2.TextBox;
+import com.googlecode.lanterna.gui2.Window;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
 public class Jotunheimr {
+	private Terminal terminal;
+	private Screen screen;
+	
+	public Jotunheimr() throws IOException {
+		terminal = new DefaultTerminalFactory().createTerminal();
+		screen = new TerminalScreen(terminal);
+		screen.startScreen();
+
+		// Create window to hold the panel
+		BasicWindow window = new BasicWindow();
+		window.setHints(Arrays.asList(Window.Hint.FULL_SCREEN, Window.Hint.NO_DECORATIONS));
+		
+		TextBox textBox = new TextBox();
+		
+		Panel mainPanel = new Panel();
+		mainPanel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
+		mainPanel.addComponent(textBox);
+		
+		window.setComponent(mainPanel);
+		
+		// Create gui and start gui
+		MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLUE));
+		gui.addWindowAndWait(window);
+	}
+	
     public static void main(String[] args) {
+    	try {
+			Jotunheimr j = new Jotunheimr();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
     }
 }
