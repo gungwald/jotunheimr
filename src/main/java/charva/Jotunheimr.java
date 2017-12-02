39e3f23bec5a0f8b0833db58be63aa2500e764bf
Bill Chatfield <bill_chatfield@yahoo.com>
2017-12-01 21:33:04 -0500

    Fix problems with charva

---
+++ src/main/java/charva/Jotunheimr.java
@@ -0,0 +1,10 @@
+package charva;
+
+public class Jotunheimr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/lanterna/Jotunheimr.java
+++ src/main/java/lanterna/Jotunheimr.java
@@ -1,8 +1,11 @@
+package lanterna;
 import java.io.IOException;
 import java.util.Arrays;
 
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.gui2.Button;
 import com.googlecode.lanterna.gui2.DefaultWindowManager;
 import com.googlecode.lanterna.gui2.Direction;
 import com.googlecode.lanterna.gui2.EmptySpace;
@@ -10,6 +13,7 @@ import com.googlecode.lanterna.gui2.LinearLayout;
 import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
 import com.googlecode.lanterna.gui2.Panel;
 import com.googlecode.lanterna.gui2.TextBox;
+import com.googlecode.lanterna.gui2.TextBox.Style;
 import com.googlecode.lanterna.gui2.Window;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
@@ -29,10 +33,17 @@ public class Jotunheimr {
 		BasicWindow window = new BasicWindow();
 		window.setHints(Arrays.asList(Window.Hint.FULL_SCREEN, Window.Hint.NO_DECORATIONS));
 		
-		TextBox textBox = new TextBox();
+		Button openButton = new Button("Open");
+		Button saveButton = new Button("Save");
 		
-		Panel mainPanel = new Panel();
-		mainPanel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
+		TextBox textBox = new TextBox(new TerminalSize(80,24),Style.MULTI_LINE);
+		
+		Panel buttonPanel = new Panel(new LinearLayout(Direction.HORIZONTAL));
+		buttonPanel.addComponent(openButton);
+		buttonPanel.addComponent(saveButton);
+		
+		Panel mainPanel = new Panel(new LinearLayout(Direction.VERTICAL));
+		mainPanel.addComponent(buttonPanel);
 		mainPanel.addComponent(textBox);
 		
 		window.setComponent(mainPanel);
@@ -44,6 +55,7 @@ public class Jotunheimr {
 	
     public static void main(String[] args) {
     	try {
+			@SuppressWarnings("unused")
 			Jotunheimr j = new Jotunheimr();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
